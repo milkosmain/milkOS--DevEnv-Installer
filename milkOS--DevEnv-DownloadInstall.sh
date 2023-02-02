114da0679e40d7cef8d87f5c165f93ed8174c903
Andrew Baker <mrandrewjbaker+milkos@gmail.com>
2023-02-02 03:28:30 -0500

    Fixing cowsay in DownloadInstall script

--- milkOS--DevEnv-DownloadInstall.sh
+++ milkOS--DevEnv-DownloadInstall.sh
@@ -11,13 +11,14 @@ echo -en "Welcome to the milkOS Development Environment Installer Download Scrip
 echo -en " ________________________________\n"
 echo -en "< Have you milked your OS today? >\n"
 echo -en " --------------------------------\n"
-echo -en "        \\   ^__^\n"
-echo -en "         \\  (oo)\\_______\n"
-echo -en "            (__)\\       )\\/\\\n"
-echo -en "                ||----w |\n"
-echo -en "                ||     ||\n"
-echo -en "_/|_/|\__  _\|_/|\__  _\|_/|\__  _\|\n"
-
+echo -en "        \\   ^__^ \n"
+echo -en "         \\  (oo)\\_______ \n"
+echo -en "            (__)\\       )\\/\ \n"
+echo -en "                ||----w || \n"
+echo -en "                ||      || \n"
+echo -en "                dd      dd \n"
+echo -en "_/|_/|\__  _\|_/|\__  _\|_/|\__  _\| \n"
+echo -en "\n\n"
 
 echo -en "Beginning download of other resources...\n"
 
