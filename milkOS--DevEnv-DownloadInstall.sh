dfcb6186dce5191e6794d79e807369d8a4078fb5
Andrew Baker <mrandrewjbaker+milkos@gmail.com>
2023-02-02 02:33:28 -0500

    Adding DownloadInstall script to debug / test use

---
+++ milkOS--DevEnv-DownloadInstall.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+DOWNLOAD_HOST_URL="https://github.com/"
+DOWNLOAD_STORAGE_PATH="./.milkOS--DevEnv-Installer.d/"
+
+echo -en "Welcome to the milkOS Development Environment Install Script\n"
+echo -en "Beginning download of other resources...\n"
