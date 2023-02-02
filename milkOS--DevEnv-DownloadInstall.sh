6fed919be6c50b2f76597926d1686ada8f9a2899
Andrew Baker <mrandrewjbaker+milkos@gmail.com>
2023-02-02 03:25:26 -0500

    Update DownloadInstall script with download url root and some cowsay

--- milkOS--DevEnv-DownloadInstall.sh
+++ milkOS--DevEnv-DownloadInstall.sh
@@ -1,7 +1,23 @@
 #!/bin/sh
+clear
 
-DOWNLOAD_HOST_URL="https://github.com/"
+DOWNLOAD_HOST_URL_ROOT="https://raw.githubusercontent.com/milkosmain/milkOS--DevEnv-Installer/main/"
 DOWNLOAD_STORAGE_PATH="./.milkOS--DevEnv-Installer.d/"
+PACKAGE_LIST=""
 
-echo -en "Welcome to the milkOS Development Environment Install Script\n"
+echo -en "Welcome to the milkOS Development Environment Installer Download Script\n"
+
+
+echo -en " ________________________________\n"
+echo -en "< Have you milked your OS today? >\n"
+echo -en " --------------------------------\n"
+echo -en "        \\   ^__^\n"
+echo -en "         \\  (oo)\\_______\n"
+echo -en "            (__)\\       )\\/\\\n"
+echo -en "                ||----w |\n"
+echo -en "                ||     ||\n"
+echo -en "_/|_/|\__  _\|_/|\__  _\|_/|\__  _\|\n"
+
+
 echo -en "Beginning download of other resources...\n"
+
