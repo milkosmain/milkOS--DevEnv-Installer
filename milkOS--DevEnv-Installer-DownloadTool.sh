9913e24742a07d39c17b269a6074f0c58e0b9cf3
Andrew Baker <mr.andrewjbaker+milkos@gmail.com>
2023-02-06 02:15:59 -0500

    Begin adding resources, printy Beginning main installer file

--- milkOS--DevEnv-Installer-DownloadTool.sh
+++ milkOS--DevEnv-Installer-DownloadTool.sh
@@ -4,53 +4,54 @@ clear
 DEBUG=0
 CLEANUP=0
 
-DOWNLOAD_HOST_URL_ROOT="https://raw.githubusercontent.com/milkosmain/milkOS--DevEnv-Installer/main/"
-DOWNLOAD_STORAGE_PATH="./.milkOS--DevEnv-Installer.d/"
+MILKOS_DOWNLOAD_HOST_URL_ROOT="https://raw.githubusercontent.com/milkosmain/milkOS--DevEnv-Installer/main/"
+THIS_SCRIPT_FILENAME="milkOS--DevEnv-Installer-DownloadTool.sh"
+MILKOS_DOWNLOAD_STORAGE_PATH="./.milkOS--DevEnv-Installer.d/"
 PACKAGE_LIST=""
 
 
 function milkOSDevEnvInstallerDownloadTool__header() {
-  milkOSDevEnvInstallDownloader__header__terminal_width=$(stty size | awk '{print $2}')
-  milkOSDevEnvInstallDownloader__header__terminal_width=$((${milkOSDevEnvInstallDownloader__header__terminal_width} - 2))
-  milkOSDevEnvInstallDownloader__header_border_character_topleft="╭"
-  milkOSDevEnvInstallDownloader__header_border_character_topright="╮"
-  milkOSDevEnvInstallDownloader__header_border_character_bottomleft="╰"
-  milkOSDevEnvInstallDownloader__header_border_character_bottomright="╯"
-  milkOSDevEnvInstallDownloader__header_border_character_verticaledge="│"
-  milkOSDevEnvInstallDownloader__header_border_character_horizontaledge="─"
+  milkOSDevEnvInstallerDownloadTool__header__terminal_width=$(stty size | awk '{print $2}')
+  milkOSDevEnvInstallerDownloadTool__header__terminal_width=$((${milkOSDevEnvInstallerDownloadTool__header__terminal_width} - 2))
+  milkOSDevEnvInstallerDownloadTool__header_border_character_topleft="╭"
+  milkOSDevEnvInstallerDownloadTool__header_border_character_topright="╮"
+  milkOSDevEnvInstallerDownloadTool__header_border_character_bottomleft="╰"
+  milkOSDevEnvInstallerDownloadTool__header_border_character_bottomright="╯"
+  milkOSDevEnvInstallerDownloadTool__header_border_character_verticaledge="│"
+  milkOSDevEnvInstallerDownloadTool__header_border_character_horizontaledge="─"
 
-  milkOSDevEnvInstallDownloader__header_border_horizontaledge=$(printf "%0.s${milkOSDevEnvInstallDownloader__header_border_character_horizontaledge}" $(seq 1 ${milkOSDevEnvInstallDownloader__header__terminal_width}))
-  milkOSDevEnvInstallDownloader__header_border_top=$(printf "%s%s%s" "${milkOSDevEnvInstallDownloader__header_border_character_topleft}" "${milkOSDevEnvInstallDownloader__header_border_horizontaledge}" "${milkOSDevEnvInstallDownloader__header_border_character_topright}")
+  milkOSDevEnvInstallerDownloadTool__header_border_horizontaledge=$(printf "%0.s${milkOSDevEnvInstallerDownloadTool__header_border_character_horizontaledge}" $(seq 1 ${milkOSDevEnvInstallerDownloadTool__header__terminal_width}))
+  milkOSDevEnvInstallerDownloadTool__header_border_top=$(printf "%s%s%s" "${milkOSDevEnvInstallerDownloadTool__header_border_character_topleft}" "${milkOSDevEnvInstallerDownloadTool__header_border_horizontaledge}" "${milkOSDevEnvInstallerDownloadTool__header_border_character_topright}")
 
-    milkOSDevEnvInstallDownloader__header_messages=""
+    milkOSDevEnvInstallerDownloadTool__header_messages=""
   for argument_message in "$@"; do
-    milkOSDevEnvInstallDownloader__header_message_width=${#argument_message}
-    milkOSDevEnvInstallDownloader__header_border_middle_message_padding_width=$(( $(( ${milkOSDevEnvInstallDownloader__header__terminal_width} - ${milkOSDevEnvInstallDownloader__header_message_width} )) / 2 ))
-    milkOSDevEnvInstallDownloader__header_border_middle_message_padding=$(printf "%0.s " $(seq 1 $(( $(( ${milkOSDevEnvInstallDownloader__header__terminal_width} - ${milkOSDevEnvInstallDownloader__header_message_width} )) / 2 )) ))
-    milkOSDevEnvInstallDownloader__header_border_middle_message_padding_right=${milkOSDevEnvInstallDownloader__header_border_middle_message_padding}
-    milkOSDevEnvInstallDownloader__header_border_middle_message_padding_left=${milkOSDevEnvInstallDownloader__header_border_middle_message_padding}
-    if [ $(( $(( ${milkOSDevEnvInstallDownloader__header__terminal_width} - ${milkOSDevEnvInstallDownloader__header_message_width} )) % 2 )) -eq 1 ]; then
-      milkOSDevEnvInstallDownloader__header_border_middle_message_padding_right="${milkOSDevEnvInstallDownloader__header_border_middle_message_padding_right} "
+    milkOSDevEnvInstallerDownloadTool__header_message_width=${#argument_message}
+    milkOSDevEnvInstallerDownloadTool__header_border_middle_message_padding_width=$(( $(( ${milkOSDevEnvInstallerDownloadTool__header__terminal_width} - ${milkOSDevEnvInstallerDownloadTool__header_message_width} )) / 2 ))
+    milkOSDevEnvInstallerDownloadTool__header_border_middle_message_padding=$(printf "%0.s " $(seq 1 $(( $(( ${milkOSDevEnvInstallerDownloadTool__header__terminal_width} - ${milkOSDevEnvInstallerDownloadTool__header_message_width} )) / 2 )) ))
+    milkOSDevEnvInstallerDownloadTool__header_border_middle_message_padding_right=${milkOSDevEnvInstallerDownloadTool__header_border_middle_message_padding}
+    milkOSDevEnvInstallerDownloadTool__header_border_middle_message_padding_left=${milkOSDevEnvInstallerDownloadTool__header_border_middle_message_padding}
+    if [ $(( $(( ${milkOSDevEnvInstallerDownloadTool__header__terminal_width} - ${milkOSDevEnvInstallerDownloadTool__header_message_width} )) % 2 )) -eq 1 ]; then
+      milkOSDevEnvInstallerDownloadTool__header_border_middle_message_padding_right="${milkOSDevEnvInstallerDownloadTool__header_border_middle_message_padding_right} "
     fi
-    milkOSDevEnvInstallDownloader__header_border_middle=$(printf "%s%s%s" "${milkOSDevEnvInstallDownloader__header_border_character_verticaledge}" "${milkOSDevEnvInstallDownloader__header_border_middle_message_padding_left}" "${argument_message}" "${milkOSDevEnvInstallDownloader__header_border_middle_message_padding_right}" "${milkOSDevEnvInstallDownloader__header_border_character_verticaledge}")
-    milkOSDevEnvInstallDownloader__header_messages="${milkOSDevEnvInstallDownloader__header_messages}${milkOSDevEnvInstallDownloader__header_border_middle}\n"
+    milkOSDevEnvInstallerDownloadTool__header_border_middle=$(printf "%s%s%s" "${milkOSDevEnvInstallerDownloadTool__header_border_character_verticaledge}" "${milkOSDevEnvInstallerDownloadTool__header_border_middle_message_padding_left}" "${argument_message}" "${milkOSDevEnvInstallerDownloadTool__header_border_middle_message_padding_right}" "${milkOSDevEnvInstallerDownloadTool__header_border_character_verticaledge}")
+    milkOSDevEnvInstallerDownloadTool__header_messages="${milkOSDevEnvInstallerDownloadTool__header_messages}${milkOSDevEnvInstallerDownloadTool__header_border_middle}\n"
   done
 
-  milkOSDevEnvInstallDownloader__header_border_bottom=$(printf "%s%s%s" "${milkOSDevEnvInstallDownloader__header_border_character_bottomleft}" "${milkOSDevEnvInstallDownloader__header_border_horizontaledge}" "${milkOSDevEnvInstallDownloader__header_border_character_bottomright}")
+  milkOSDevEnvInstallerDownloadTool__header_border_bottom=$(printf "%s%s%s" "${milkOSDevEnvInstallerDownloadTool__header_border_character_bottomleft}" "${milkOSDevEnvInstallerDownloadTool__header_border_horizontaledge}" "${milkOSDevEnvInstallerDownloadTool__header_border_character_bottomright}")
 
-  echo -en "${milkOSDevEnvInstallDownloader__header_border_top}\n"
-  echo -en "${milkOSDevEnvInstallDownloader__header_messages}"
-  echo -en "${milkOSDevEnvInstallDownloader__header_border_bottom}\n"
+  echo -en "${milkOSDevEnvInstallerDownloadTool__header_border_top}\n"
+  echo -en "${milkOSDevEnvInstallerDownloadTool__header_messages}"
+  echo -en "${milkOSDevEnvInstallerDownloadTool__header_border_bottom}\n"
 }
 
 function milkOSDevEnvInstallerDownloadTool__createDownloadStorageDir() {
   echo -en "Creating download storage...\n\n"
-  if [ -d "$DOWNLOAD_STORAGE_PATH" ]; then
+  if [ -d "$MILKOS_DOWNLOAD_STORAGE_PATH" ]; then
     echo -en "Download storage already exists!\n"
     echo -en "Skipping creation.\n"
   else
-    mkdir "$DOWNLOAD_STORAGE_PATH"
-    if [ ! -d "$DOWNLOAD_STORAGE_PATH" ]; then
+    mkdir "$MILKOS_DOWNLOAD_STORAGE_PATH"
+    if [ ! -d "$MILKOS_DOWNLOAD_STORAGE_PATH" ]; then
       echo -en "Unable to create download storage!\n"
       echo -en "Try running this script as root.\n"
       exit 1
@@ -62,18 +63,40 @@ function milkOSDevEnvInstallerDownloadTool__createDownloadStorageDir() {
 
 }
 
-function milkOSDevEnvInstallerDownloadTool__download_milkOSDevEnvInstaller() {
-  echo -en "Downloading 'milkOS--DevEnv-Installer'...\n\n"
+function milkOSDevEnvInstallerDownloadTool__downloadResource() {
+  local resourceName="$1"
+  echo -en "Downloading '${resourceName}'...\n\n"
+  echo -en "From: ${MILKOS_DOWNLOAD_HOST_URL_ROOT}/milkosmain/milkOS--DevEnv-Installer/${resourceName}\n\n"
+  
+  echo -en "${MILKOS_DOWNLOAD_STORAGE_PATH}${resourceName}\n\n"
+  
+  downloadResult=$(wget -q "${MILKOS_DOWNLOAD_HOST_URL_ROOT}/milkosmain/milkOS--DevEnv-Installer/main/${resourceName}" -P "${MILKOS_DOWNLOAD_STORAGE_PATH}" -O "${MILKOS_DOWNLOAD_STORAGE_PATH}${resourceName}" )
 
+  echo -en "${downloadResult}"
+
+  MILKOS_DOWNLOAD_STORAGE_CONTENTS=$(ls "./${MILKOS_DOWNLOAD_STORAGE_PATH}")
+
+  echo -en "${MILKOS_DOWNLOAD_STORAGE_CONTENTS}\n"
+
+  echo -en "\n${MILKOS_DOWNLOAD_STORAGE_PATH}${resourceName}\n"
+
+
+  if [ ! -f "${MILKOS_DOWNLOAD_STORAGE_PATH}${resourceName}" ]; then
+    echo -en "Unable to download resource ${resourceName}!\n"
+    echo -en "Exiting.\n"
+    exit 1
+  else
+    echo -en "Downloaded '${resourceName}'\n"
+  fi
 }
 
 function milkOSDevEnvInstallerDownloadTool__main() {
-  DEV_ENV_INSTALL_DOWNLOADER__COMMAND=$(cat ~/.ash_history | grep '/bin/sh' | tail -1)
-  DEV_ENV_INSTALL_DOWNLOADER__URL_ROOT=$(echo "$DEV_ENV_INSTALL_DOWNLOADER__COMMAND" | sed -n -e "s/^.*-O- //p; s/\/milkosmain.*//p" | tail -1)
+  milkOSDevEnvInstallerDownloadTool__command=$(cat ~/.ash_history | grep '/bin/sh' | tail -1)
+  milkOSDevEnvInstallerDownloadTool__command__URLRoot=$(echo "$milkOSDevEnvInstallerDownloadTool__command" | sed -n -e "s/^.*-O- //p; s/\/milkosmain.*//p" | tail -1)
+  MILKOS_DOWNLOAD_HOST_URL_ROOT=$milkOSDevEnvInstallerDownloadTool__command__URLRoot
 
-  if [ "$0" = "-dev" ]; then
+  if [ "$0" = "-debug" ]; then
     DEBUG="1"
-    DOWNLOAD_HOST_URL_ROOT=$DEV_ENV_INSTALL_DOWNLOADER__URL_ROOT
   fi
 
   if [ "$1" = "-cleanup" ]; then
@@ -94,9 +117,23 @@ function milkOSDevEnvInstallerDownloadTool__main() {
   milkOSDevEnvInstallerDownloadTool__createDownloadStorageDir
 
   if [[ "$CLEANUP" = "1" ]]; then
-    rm -rf "${DOWNLOAD_STORAGE_PATH}"
-    rm -rf ./milkOS--DevEnv-Installer-Download.sh
+    echo -en "Cleaning up...\n"
+    rm -rf "${MILKOS_DOWNLOAD_STORAGE_PATH}"
+    if [ -d "$MILKOS_DOWNLOAD_STORAGE_PATH" ]; then
+      echo -en "Could not clean download storage!\n"
+    else
+      echo -en "Cleaned download storage!\n"
+    fi
+    rm -rf "./${THIS_SCRIPT_FILENAME}"
+    if [ -f "./${THIS_SCRIPT_FILENAME}" ]; then
+      echo -en "Could not clean script file!\n"
+    else
+      echo -en "Cleaned script file!\n"
+    fi
+
   fi
+
+  milkOSDevEnvInstallerDownloadTool__downloadResource "milkOS--DevEnv-Installer.sh"
 }
 
 milkOSDevEnvInstallerDownloadTool__main $@
---
+++ milkOS--DevEnv-Installer.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+echo -en "Welcome to the milkOS DevEnv Installer\n"
+echo -en "This is an installer to help you configure your environment for Development of milkOS"
+echo -en ", as well as applications, programs, and services for milkOS (or any derivatives).\n"
+echo -en "Thank you for any time you may put into improving milkOS!\n"
+
+echo -en "Checking for resources...\n"
+
+echo -en "Gathering buckets\n"
+sleep 4
+echo -en "Herdding an OS\n"
+sleep 4
+echo -en "Shoveling sh*t\n"
+sleep 4
+echo -en "Getting a stool\n"
+sleep 4
+echo -en "Breathing in the crisp server-farm air\n"
+sleep 4
+echo -en "Time to milk the OS\n"
+echo -en "\n"
+
+echo -en "This script will ask you a few questions, mainly so that we know what programs you need\n\n"
+
+echo -en "Select your virtualization environment (type number and hit enter):\n"
+echo -en "1 - VirtualBox\n"
+echo -en "2 - VMware\n"
+echo -en "3 - Hyper-V\n"
+echo -en "
+
+echo -en "Select milkOS use case (type number and hit enter):\n"
+echo -en "1 - Home\n"
+echo -en "2 - Serve\n"
+echo -en "3 - Workspace\n"
+
+sleep 4
