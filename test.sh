f56f023836b2145eed03534134bc054174db85f4
Andrew Baker <mr.andrewjbaker+milkos@gmail.com>
2023-02-08 23:13:29 -0500

    Rebuilt printy v1 with banner

--- test.sh
+++ test.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
-echo -en "\n\n"
+clear
 
 echo "moosh : MilkOS Open Shell - ver 0:23.02.06"
-
 echo "testing printy() from milkOS system"
-
-. ./resources/milkOSSystem/printy
 echo -en "\n"
 
 
+
+## testing ##
+. ./resources/milkOSSystem/printy
 # printy fg@white bg@mint+bold "What in the moo is this?\n"
-printy @banner title="This is the title I want" width@100%
+# printy @style fg=white bg=mint+bold "What in the moo is this?\n"
+printy bg@orange fg@black @continue
+printy @banner title="You need a better OS" message="I sure hope this looks cool!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo -en "\e[0m"
